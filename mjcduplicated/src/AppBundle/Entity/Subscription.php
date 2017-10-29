49b36a56b37c72dea07648e8e9b0cc2730795fcd
ludulle <maclodin@gmail.com>
2017-10-29 14:34:34 +0100

    Entities User & Subscription

---
+++ mjcduplicated/src/AppBundle/Entity/Subscription.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Subscription
+ *
+ * @ORM\Table(name="subscription")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SubscriptionRepository")
+ */
+class Subscription
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="startAt", type="datetime")
+     */
+    private $startAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="finishAt", type="datetime")
+     */
+    private $finishAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duration", type="integer", nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="subscriptionAt", type="datetime")
+     */
+    private $subscriptionAt;
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set startAt
+     *
+     * @param \DateTime $startAt
+     *
+     * @return Subscription
+     */
+    public function setStartAt($startAt)
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    /**
+     * Get startAt
+     *
+     * @return \DateTime
+     */
+    public function getStartAt()
+    {
+        return $this->startAt;
+    }
+
+    /**
+     * Set finishAt
+     *
+     * @param \DateTime $finishAt
+     *
+     * @return Subscription
+     */
+    public function setFinishAt($finishAt)
+    {
+        $this->finishAt = $finishAt;
+
+        return $this;
+    }
+
+    /**
+     * Get finishAt
+     *
+     * @return \DateTime
+     */
+    public function getFinishAt()
+    {
+        return $this->finishAt;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     *
+     * @return Subscription
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return int
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set subscriptionAt
+     *
+     * @param \DateTime $subscriptionAt
+     *
+     * @return Subscription
+     */
+    public function setSubscriptionAt($subscriptionAt)
+    {
+        $this->subscriptionAt = $subscriptionAt;
+
+        return $this;
+    }
+
+    /**
+     * Get subscriptionAt
+     *
+     * @return \DateTime
+     */
+    public function getSubscriptionAt()
+    {
+        return $this->subscriptionAt;
+    }
+
+}
